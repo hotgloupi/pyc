c70cda31af12146c5a850d3805330226c314259a
Raphaël Londeix <raphael.londeix@infinit.io>
2017-11-04 10:22:53 +0000

    wip

---
+++ lib/builtins.py
@@ -0,0 +1,12 @@
+# builtins
+
+from __pyc__ import extern, int, void_p, size_t, ssize_t
+
+@extern
+def write(filedes: int, buf: void_p, size: size_t) -> ssize_t:
+    pass
+
+def print(s):
+    write(1, s, 5)
+
+print("coucou")
